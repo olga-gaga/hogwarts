ae14c129bcbbd08a58aeab14c3693a0e890d713c
olga-gaga <garbuz9112000@gmail.com>
2019-09-30 13:51:32 +0300

    Удалены комментарии в коде, новый фон error404

--- book-script.js
+++ book-script.js
@@ -1,17 +1,6 @@
 import {styleLoad} from './style-loading.js';
 import {getQueryVariable} from './queryVarieble.js';
 const url = 'https://api-euwest.graphcms.com/v1/ck0djr5sr0g7f01d0ayv93gt1/master';
-/*function getQueryVariable(variable) 
-{
-   let query = window.location.search.substring(1);
-   let vars = query.split("&");
-   for (let i=0;i<vars.length;i++) {
-       let pair = vars[i].split("=");
-       if(pair[0] === variable){return pair[1];}
-   }
-   return(false);
-}*/
-
 let lastName = getQueryVariable('st'); 
 const studentQuery = `
       {
@@ -44,15 +33,6 @@ axios.post(url, {query: studentQuery})
     student = response.data.data.students[0];
     console.log(student);
     styleLoad(student, 0)
-   /* document.body.style.background = "url(" + student.house.backgroundImg.url + ")";
-    document.body.style.backgroundSize = "cover";
-    let mainPage = document.getElementById("main-page");
-    mainPage.classList.toggle(student.house.houseName);
-
-    let titles = document.getElementById("titles");
-   
-    titles.innerHTML += ` <h1 id="profile-name"> ${student.firstName} ${student.lastName} </h1> `;
-    */
     let tbody = document.getElementById("add-books");
    for (let subjOnCourse of student.subjectOnCourses) {
      for(let i = 0; i < subjOnCourse.courseBooks.length; i++){
@@ -63,16 +43,5 @@ axios.post(url, {query: studentQuery})
 else {tbody.innerHTML +=`<tr><td> ${itemBook.title} </td><td> ${itemBook.author} </td> </tr>`}
       }
     }
-
-   /*for (let subjOnCourse of student.course.subjectOnCourses) {
-     for(let itemBook of subjOnCourse.courseBooks){
-         tbody.innerHTML += `<tr> <td> ${subjOnCourse.subject.subjectName} </td> 
-          <td> ${itemBook.title} </td>
-          <td> ${itemBook.author} </td>
-
-          </tr>`
-      }
-    }*/
-
 })  
 
--- student-script.js
+++ student-script.js
@@ -1,17 +1,6 @@
 import {styleLoad} from './style-loading.js';
 import {getQueryVariable} from './queryVarieble.js';
 const url = 'https://api-euwest.graphcms.com/v1/ck0djr5sr0g7f01d0ayv93gt1/master';
-/*function getQueryVariable(variable) 
-{
-   let query = window.location.search.substring(1);
-   let vars = query.split("&");
-   for (let i=0;i<vars.length;i++) {
-       let pair = vars[i].split("=");
-       if(pair[0] === variable){return pair[1];}
-   }
-   return(false);
-}*/
-
 let lastName = getQueryVariable('st'); 
 const studentQuery = `
       {
@@ -47,40 +36,23 @@ axios.post(url, {query: studentQuery})
       window.location.replace('error404.html');
     }
     styleLoad(student, 0)
-    /*document.body.style.background = "url(" + student.house.backgroundImg.url + ")";
-    document.body.style.backgroundSize = "cover";
-    let mainPage = document.getElementById("main-page");
-    mainPage.classList.toggle(student.house.houseName);
-
-    let titles = document.getElementById("titles");
-   
-     titles.innerHTML += ` <h1 id="profile-name"> ${student.firstName} ${student.lastName} </h1> 
-          <h2 id="house-name"> ${student.house.houseName}</h2>`;
-    */
-      let img = document.getElementById("img");
-      img.innerHTML += `<img id="profile-photo" src="${student.img.url}"/> `;
-      let a = document.getElementsByClassName("sections");
-      for (let item of a) {
-          item.href += `?st=${lastName}`;
-      }
-      let timeLeft;
-      let taskList = document.getElementById("task-list");
-      for (let item of student.subjectOnCourses){
-        for (let task of item.tasks){
-          let compDate = new Date(task.completionDate);
-          timeLeft = Math.ceil((compDate.getTime() - Date.now()) / (1000 * 3600 * 24));
-          if (timeLeft > 0 && timeLeft <= 3) {
-            taskList.innerHTML += `<li> <span>${item.subject.subjectName}</span>: ${task.task}</li>`
-         }
+    let img = document.getElementById("img");
+    img.innerHTML += `<img id="profile-photo" src="${student.img.url}"/> `;
+    let a = document.getElementsByClassName("sections");
+    for (let item of a) {
+        item.href += `?st=${lastName}`;
+    }
+    let timeLeft;
+    let taskList = document.getElementById("task-list");
+    for (let item of student.subjectOnCourses){
+      for (let task of item.tasks){
+        let compDate = new Date(task.completionDate);
+        timeLeft = Math.ceil((compDate.getTime() - Date.now()) / (1000 * 3600 * 24));
+        if (timeLeft > 0 && timeLeft <= 3) {
+          taskList.innerHTML += `<li> <span>${item.subject.subjectName}</span>: ${task.task}</li>`
         }
       }
-      /*let compDate = new Date(student.subjectOnCourses[3].tasks[0].completionDate);
-
-      let timeDiff = compDate.getTime() - Date.now();
-      if (timeDiff > 0) {
-        console.log(Math.ceil(timeDiff / (1000 * 3600 * 24)));
-      }*/
-      //console.log(Date.now() - student.course.subjectOnCourses[3].subject.tasks[0].completionDate);
+    }
 })	
 
 

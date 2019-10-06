5e48d105419f22cbf8e7256da7c0d248c679a73d
olga-gaga <garbuz9112000@gmail.com>
2019-10-06 18:04:30 +0300

    tasks styleload

--- task-script.js
+++ task-script.js
@@ -73,7 +73,7 @@ let student;
 axios.post(url, {query: studentQuery})
 .then(response => {
   student = response.data.data.students[0];
-  console.log(student);
+  styleLoad(student, 0);
   let taskList = document.getElementById("task-list");
   let compTaskList = document.getElementById("completed-task-list");
   let arrTasks = student.taskForStudents;

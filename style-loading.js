707bf14bb0dde1bb26032084ee36045b8cfb0153
olga-gaga <garbuz9112000@gmail.com>
2019-11-20 15:10:34 +0200

    house2

--- style-loading.js
+++ style-loading.js
@@ -5,15 +5,15 @@ export function styleLoad(student, studentBool, houseBool) {
   	mainPage.classList.toggle(student.house.houseName);
   	let titles = document.getElementById("titles");
   	if (studentBool) {
-  		titles.innerHTML += ` <h1 id="profile-name"> <a href="index.html?st="${student.lastName}"> ${student.firstName} ${student.lastName} </a> </h1> `;
+  		titles.innerHTML += ` <h1 id="profile-name"> <a href="index.html?st=${student.lastName}"> ${student.firstName} ${student.lastName} </a> </h1> `;
   	}
   	if(houseBool) {
   		switch (houseBool) {
   			case 1: 
-  				titles.innerHTML += `<h1 id="house-name"><a href="index.html?st="${student.lastName}"> ${student.house.houseName}</a> </h1>`;
+  				titles.innerHTML += `<h1 id="house-name"><a href="index.html?st=${student.lastName}"> ${student.house.houseName}</a> </h1>`;
   				break;
   			case 2:
-  				titles.innerHTML += `<h1 id="house-name"><a href="index.html?st="${student.lastName}"> ${student.house.houseName}</a> </h1>`;
+  				titles.innerHTML += `<h1 id="house-name"><a href="index.html?st=${student.lastName}"> ${student.house.houseName}</a> </h1>`;
   		}
   	}
 }

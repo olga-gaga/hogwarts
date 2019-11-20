2aaf06b8ba475b48dfe3d8e62f6ba64bc8802fb5
olga-gaga <garbuz9112000@gmail.com>
2019-11-20 15:17:46 +0200

    house5

--- house-script.js
+++ house-script.js
@@ -3,33 +3,6 @@ import {getQueryVariable} from './queryVarieble.js';
 const canvas = document.getElementById("TheHouseCup");
 const ctx = canvas.getContext("2d");
 const url = 'https://api-euwest.graphcms.com/v1/ck0djr5sr0g7f01d0ayv93gt1/master';
-
-let rating = [];
-let color = [];
-let houseNames = [];
-function getQueryVariable(variable) 
-{
-   let query = window.location.search.substring(1);
-   let vars = query.split("&");
-   for (let i=0;i<vars.length;i++) {
-       let pair = vars[i].split("=");
-       if(pair[0] === variable){return pair[1];}
-   }
-   return(false);
-}
-
-function styleLoad(student, house) {
-    document.body.style.background = "url(" + student.house.backgroundImg.url + ")";
-    document.body.style.backgroundSize = "cover";
-    let mainPage = document.getElementById("main-page");
-    mainPage.classList.toggle("Gryffindor");
-    let titles = document.getElementById("titles");
-    //titles.innerHTML += ` <h1 id="profile-name"> <a href="index.html?st="${student.lastName}"> ${student.firstName} ${student.lastName} </a> </h1> `;
-    if (house) {
-      titles.innerHTML += `<h1 id="house-name"> ${student.house.houseName}</h2>`;
-    }
-}
-
 let lastName = getQueryVariable('st'); 
 const studentQuery = `
     {
